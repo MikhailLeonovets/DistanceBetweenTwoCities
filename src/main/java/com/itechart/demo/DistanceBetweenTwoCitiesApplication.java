ba41b112d15ff2cbc011a3b12ea96d1ef6b7bf3e
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-01-23 12:38:59 +0300

    structure of spring boot app is created

---
+++ src/main/java/com/itechart/demo/DistanceBetweenTwoCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceBetweenTwoCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanceBetweenTwoCitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itechart/demo/DistanceBetweenTwoCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanceBetweenTwoCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
