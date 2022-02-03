0c16dd4b5ac3af8cec9dbff12cda62c47ab411d6
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-02-03 18:34:19 +0300

    Scheduled refreshing of the caches added

--- src/main/java/com/itechart/demo/cache/refresh/CityCacheRefresh.java
+++ src/main/java/com/itechart/demo/cache/refresh/CityCacheRefresh.java
@@ -1,4 +1,22 @@
 package com.itechart.demo.cache.refresh;
 
+import com.itechart.demo.cache.CityCache;
+import com.itechart.demo.repository.CityRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CityCacheRefresh {
+	private final CityCache cityCache;
+	private final CityRepository cityRepository;
+
+	public CityCacheRefresh(CityCache cityCache, CityRepository cityRepository) {
+		this.cityCache = cityCache;
+		this.cityRepository = cityRepository;
+	}
+
+	@Scheduled(cron = "0 0 12 * * ?")
+	public void refreshCityCache() {
+		cityCache.setCities(cityRepository.findAll());
+	}
 }
--- src/main/java/com/itechart/demo/cache/refresh/RouteCacheRefresh.java
+++ src/main/java/com/itechart/demo/cache/refresh/RouteCacheRefresh.java
@@ -1,4 +1,22 @@
 package com.itechart.demo.cache.refresh;
 
+import com.itechart.demo.cache.RouteCache;
+import com.itechart.demo.repository.RouteRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class RouteCacheRefresh {
+	private final RouteCache routeCache;
+	private final RouteRepository routeRepository;
+
+	public RouteCacheRefresh(RouteCache routeCache, RouteRepository routeRepository) {
+		this.routeCache = routeCache;
+		this.routeRepository = routeRepository;
+	}
+
+	@Scheduled(cron = "0 0 12 * * ?")
+	public void refreshRouteCache() {
+		routeCache.setRoutes(routeRepository.findAll());
+	}
 }
