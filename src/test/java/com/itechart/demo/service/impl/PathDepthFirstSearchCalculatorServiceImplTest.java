0034030a953c1483e8f1a0f588e27b1ce09750c2
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-02-18 10:07:56 +0300

    RouteSpringDataServiceTest is updated

---
+++ src/test/java/com/itechart/demo/service/impl/PathDepthFirstSearchCalculatorServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.itechart.demo.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PathDepthFirstSearchCalculatorServiceImplTest {
+
+	@Test
+	void calculatePaths() {
+	}
+}
--- src/test/java/com/itechart/demo/service/impl/database_service/spring_data/RouteSpringDataServiceTest.java
+++ src/test/java/com/itechart/demo/service/impl/database_service/spring_data/RouteSpringDataServiceTest.java
@@ -1,5 +1,6 @@
 package com.itechart.demo.service.impl.database_service.spring_data;
 
+import com.itechart.demo.repository.entity.City;
 import com.itechart.demo.repository.entity.Route;
 import com.itechart.demo.repository.spring_data.RouteRepository;
 import com.itechart.demo.service.exception.RouteNotFoundException;
@@ -13,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -59,22 +62,75 @@ class RouteSpringDataServiceTest {
 	}
 
 	@Test
-	void findRoutesByFirstCity() {
+	void canFindRoutesByFirstCity() throws RouteNotFoundException {
+		Route route = new Route();
+		City city = new City();
+		List<Route> routeList = new ArrayList<>();
+		routeList.add(route);
+		Mockito.doReturn(routeList).when(repository).findRoutesByFirstCity(city);
+		underTest.findRoutesByFirstCity(city);
+		Mockito.verify(repository).findRoutesByFirstCity(city);
 	}
 
 	@Test
-	void update() {
+	void findRoutesByFirstCityThrowsRouteNotFoundException() {
+		City city = new City();
+		List<Route> routes = new ArrayList<>();
+		Mockito.doReturn(routes).when(repository).findRoutesByFirstCity(city);
+		AssertionsForClassTypes.assertThatThrownBy(() -> underTest.findRoutesByFirstCity(city))
+				.isInstanceOf(RouteNotFoundException.class);
+	}
+
+	@Test
+	void canUpdate() throws RouteNotFoundException {
+		Route route = new Route();
+		Mockito.doReturn(Optional.of(route)).when(repository).findById(route.getId());
+		underTest.update(route);
+		Mockito.verify(repository).save(route);
+	}
+
+	@Test
+	void updateThrowsRouteNotFoundException() {
+		Route route = new Route();
+		Mockito.doReturn(Optional.empty()).when(repository).findById(route.getId());
+		AssertionsForClassTypes.assertThatThrownBy(() -> underTest.update(route))
+				.isInstanceOf(RouteNotFoundException.class);
+		Mockito.verify(repository, Mockito.never()).save(route);
+	}
+
+	@Test
+	void canFindRouteBetweenCities() throws RouteNotFoundException {
+		City firstCity = new City();
+		City secondCity = new City();
+		Route route = new Route();
+		Mockito.doReturn(Optional.of(route)).when(repository)
+				.findByFirstCityIdAndSecondCityId(firstCity.getId(), secondCity.getId());
+		underTest.findRouteBetweenCities(firstCity, secondCity);
+		Mockito.verify(repository).findByFirstCityIdAndSecondCityId(firstCity.getId(), secondCity.getId());
 	}
 
 	@Test
 	void findRouteBetweenCities() {
+		City firstCity = new City();
+		City secondCity = new City();
+		Mockito.doReturn(Optional.empty()).when(repository)
+				.findByFirstCityIdAndSecondCityId(firstCity.getId(), secondCity.getId());
+		AssertionsForClassTypes.assertThatThrownBy(() -> underTest.findRouteBetweenCities(firstCity, secondCity))
+				.isInstanceOf(RouteNotFoundException.class);
+		Mockito.verify(repository).findByFirstCityIdAndSecondCityId(firstCity.getId(), secondCity.getId());
 	}
 
 	@Test
 	void deleteById() {
+		Long id = 1L;
+		underTest.deleteById(id);
+		Mockito.verify(repository).deleteById(id);
 	}
 
 	@Test
 	void delete() {
+		Route route = new Route();
+		underTest.delete(route);
+		Mockito.verify(repository).delete(route);
 	}
 }
